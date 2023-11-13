f84a2e9f943cb1cca92696da52b8ff6cbe4aceda
erickandongo <danielle.ntsamaa@gmail.com>
2023-11-13 01:06:20 +0100

    Hey this is the django folder

---
+++ interest.py
@@ -0,0 +1,10 @@
+# principal = 1000 #initial amount
+# rate = 0.05 # interest rate
+# numyears = 5 # Number of years
+# year = 1 
+# while year <= numyears:
+#  principal = principal * (1 + rate)
+#  print(f'{year :>3d} {principal:0.2f}') #print the year  and  principal
+#  year += 1
+
+#print(round(5,3))
