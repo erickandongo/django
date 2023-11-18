a7b9bce47d05ab72ada8f7f57acb8671c7e59873
erickandongo <danielle.ntsamaa@gmail.com>
2023-11-18 01:02:24 +0100

    Built Login

--- todo_list/base/urls.py
+++ todo_list/base/urls.py
@@ -1,6 +1,11 @@
 from django.urls import path
-from . import views
+from .views import Tasklist, TaskDetail, TaskCreate, TaskUpdate, DeleteView, CustomLoginView
 
 urlpatterns = [
-    path('', views.tasklist, name ='tasks'),
+    path('login/', CustomLoginView.as_view(), name='login' ),
+    path('', Tasklist.as_view(), name ='tasks'),
+    path('task/<int:pk>/', TaskDetail.as_view(), name ='task'),
+    path('task-create/', TaskCreate.as_view(), name ='task-create'),
+    path('task-update/<int:pk>/', TaskUpdate.as_view(), name ='task-update'),
+    path('task-delete/<int:pk>/', DeleteView.as_view(), name ='task-delete'),
 ]
--- todo_list/todo_list/settings.py
+++ todo_list/todo_list/settings.py
@@ -20,7 +20,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-!yl7mmi__nca#x5qjbcgdtxpdk$balx3jsxxdrpc9bjmir6z=!'
+SECRET_KEY = 'django-insecure-ox!=#ue759pl6_13zewm+@js+jn0@k5at=ovv98pz0g)cb#^m8'
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
@@ -37,6 +37,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'base.apps.BaseConfig',
 ]
 
 MIDDLEWARE = [
